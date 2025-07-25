5f9c88895117af57062aea8e881f849b58f8b673
Aarohan Niraula <aarohan567@gmail.com>
2025-07-25 14:02:15 +0545

    Fix bug

--- app/views.py
+++ app/views.py
@@ -5,4 +5,4 @@ def about(request):
     return render(request, "about.html")
 
 def home(request):
-    return render(request, "base.html")
+    return render(request, "index.html")
--- ekthal/settings.py
+++ ekthal/settings.py
@@ -58,7 +58,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        'DIRS': [BASE_DIR / "templates"], 
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
