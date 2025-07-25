7781132aff7b217c1bac1a1351e3ed2a5318fddb
Krish Naharki <krishnaharki827@example.com>
2025-07-25 18:37:30 +0545

    About and Contact

--- ekthal/urls.py
+++ ekthal/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("register/", views.register, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
